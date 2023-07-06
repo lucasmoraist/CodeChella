61ef66718e1cf171fcb12a3f13939cc019185beb
lucasmoraist <luksmnt1101@gmail.com>
2023-07-06 15:57:59 -0300

    Template da api spring

---
+++ api/codeshella/codeshella/src/main/java/br/com/codeshella/CodeshellaApplication.java
@@ -0,0 +1,13 @@
+package br.com.codeshella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeshellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeshellaApplication.class, args);
+	}
+
+}
