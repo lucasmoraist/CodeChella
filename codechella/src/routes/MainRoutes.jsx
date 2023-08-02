c5ebfabfcc630f7f05a1aa5b1d3aa7b9150e1a07
lucasmoraist <luksmnt1101@gmail.com>
2023-08-02 14:33:16 -0300

    Criando nova rota

--- codechella/src/routes/MainRoutes.jsx
+++ codechella/src/routes/MainRoutes.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route} from 'react-router-dom';
 import Home from '../components/home/Home'
 import Experiencia from "../components/experiencia/Experiencia";
+import Mapa from "../components/mapa/Mapa";
 
 export default function MainRoutes(){
     return(
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/experiencia" element={<Experiencia/>}/>
+            <Route path="/setores" element={<Mapa/>}/>
         </Routes>
     )
 }
