13b5498eef021d174f17adecd242f6e24aeb8e3e
lucasmoraist <luksmnt1101@gmail.com>
2024-04-11 14:30:54 -0300

    feat: salvando dados em memória

--- src/pages/forms/Forms.jsx
+++ src/pages/forms/Forms.jsx
@@ -17,13 +17,11 @@ const Forms = () => {
     const [diaFestival, setDiaFestival] = useState('');
     const [erroCPF, setErroCPF] = useState('');
     const [erroIdade, setErroIdade] = useState('');
-    const [mensagem, setMensagem] = useState('');
     const [enviado, setEnviado] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        
         if (!ValidaCpf(cpf)) {
             setErroCPF('CPF inválido');
             return;
@@ -31,62 +29,9 @@ const Forms = () => {
             setErroCPF('');
         }
         
-
         ShowAge(dtNasc, setErroIdade);
-
-        try {
-            //const responsePessoa = await fetch('https://api-codechella.azurewebsites.net/api/pessoa', {
-            const responsePessoa = await fetch('http://localhost:8050/api/pessoa', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    nome,
-                    email,
-                    cpf,
-                    dtNasc,
-                }),
-            });
-
-            if (responsePessoa.ok) {
-                const pessoaData = await responsePessoa.json();
-                console.log(pessoaData);
-                const id = pessoaData.id;
-
-                //const responseIngresso = await fetch('https://api-codechella.azurewebsites.net/api/ingresso', {
-                const responseIngresso = await fetch('http://localhost:8050/api/ingresso', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({
-                        diaFestival,
-                        setor,
-                        pessoa: {
-                            id: id,
-                        }
-                    }),
-                })
-
-                if (responseIngresso.ok) {
-                    const ingressoData = await responseIngresso.json();
-                    console.log('Ingresso criado:', ingressoData);
-                } else {
-                    console.error('Erro ao criar o ingresso:', responseIngresso.status);
-                }
-
-                if (responsePessoa.ok && responseIngresso.ok) {
-                    setMensagem('Dados enviados com sucesso!');
-                    setEnviado(true);
-                } else {
-                    setMensagem('Erro ao enviar os dados. Tente novamente.');
-                }
-            }
-        } catch (error) {
-            setMensagem('Erro ao enviar os dados. Tente novamente.');
-        }
-
+        
+        setEnviado(true)
     };
 
     if (enviado) {
@@ -145,8 +90,6 @@ const Forms = () => {
 
                     <button type="submit" >Enviar</button>
                 </form>
-
-                <p>{mensagem}</p>
             </div>
 
         </DivForm>
